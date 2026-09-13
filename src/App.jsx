27b28c6f0d55510bca2324d1c7f834594b8f7fbc
Mohammed Ait sidi bah <aytsidibah@gmail.com>
2026-09-13 01:55:11 +0100

    Refactor imports to use components directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './index.css';
-import Header from './hooks/Header.jsx';
-import Home from './hooks/Hero.jsx';
-import About from './hooks/About.jsx';
-import Skills from './hooks/Skills.jsx';
-import Projects from './hooks/Projects.jsx';
-import Contact from './hooks/Contact.jsx';
-import Footer from './hooks/Footer.jsx';
+import Header from './components/Header.jsx';
+import Home from './components/Hero.jsx';
+import About from './components/About.jsx';
+import Skills from './components/Skills.jsx';
+import Projects from './components/Projects.jsx';
+import Contact from './components/Contact.jsx';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
 
